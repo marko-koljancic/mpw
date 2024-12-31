ed758b2abf9e097ee18acab938ca370cede678b5
marko <marko.koljancic@outlook.com>
2024-12-31 17:35:45 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div>
+      <main>
+        <p>MPW - Starting page</p>
+      </main>
+      <footer>
+        <p>Footer content</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
